24f0fa31c1c711e5898b12d833748f2e8327bf59
brushette <brushette12@gmail.com>
2021-01-04 16:28:11 +0100

    [ADD] language support

--- src/main/java/calebcompass/calebcompass/util/MessageUtil.java
+++ src/main/java/calebcompass/calebcompass/util/MessageUtil.java
@@ -1,10 +1,5 @@
 package calebcompass.calebcompass.util;
 
-import calebcompass.calebcompass.SavePoints.SavePoint;
-import org.bukkit.Location;
-import org.bukkit.util.Vector;
-
-import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,17 +7,25 @@ public class MessageUtil {
 
 	private static final String FORMAT = "▘⬟⬟⬟⬟▙⬟⬟⬟⬟▚⬟⬟⬟⬟▛⬟⬟⬟⬟▜⬟⬟⬟⬟▝⬟⬟⬟⬟▞⬟⬟⬟⬟▟⬟⬟⬟⬟▘⬟⬟⬟⬟▙⬟⬟⬟⬟▚⬟⬟⬟⬟▛⬟⬟⬟⬟▜⬟⬟⬟⬟▝⬟⬟⬟⬟▞⬟⬟⬟⬟▟⬟⬟⬟⬟▘⬟⬟⬟⬟▙⬟⬟⬟⬟▚⬟⬟⬟⬟▛⬟⬟⬟⬟▜⬟⬟⬟⬟▝⬟⬟⬟⬟▞⬟⬟⬟⬟▟⬟⬟⬟⬟";
 
-	private static final Pattern HEX_PATTERN = Pattern.compile("&(#\\w{6})");
+    private static final Pattern HEX_PATTERN = Pattern.compile("&(#\\w{6})");
 
-	public static String colourise(String message) {
-		Matcher matcher = HEX_PATTERN.matcher(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message));
-		StringBuffer buffer = new StringBuffer();
+    public static String colourise(String message) {
+        Matcher matcher = HEX_PATTERN.matcher(net.md_5.bungee.api.ChatColor.translateAlternateColorCodes('&', message));
+        StringBuffer buffer = new StringBuffer();
 
-		while (matcher.find()) {
-			matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group(1).toUpperCase()).toString());
-		}
+        while (matcher.find()) {
+            matcher.appendReplacement(buffer, net.md_5.bungee.api.ChatColor.of(matcher.group(1).toUpperCase()).toString());
+        }
 
-		return matcher.appendTail(buffer).toString();
-	}
+        return matcher.appendTail(buffer).toString();
+    }
+
+	// replace %placeholder% with replacement for messages
+	public static String replaceVariable(String message, String placeholder, String replacement) {
+        Pattern p = Pattern.compile(placeholder);
+        Matcher m = p.matcher(message);
+        return m.replaceAll(replacement);
+    }
+
 
 }
